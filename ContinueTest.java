4f161f518f2aa980eefe8fb0948d7ce631994728
CATHAL KIRRANE - STUDENT <G00349777@gmit.ie>
2018-02-26 11:50:53 +0000

    week 21 java lab

---
+++ ContinueTest.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class ContinueTest{
+	public static void main (String[] args){
+		for(int count =1; count <=10; count++){
+			if(count != 5){
+				System.out.println(count);
+			}
+		}
+	}
+}
