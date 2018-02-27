d9f549b30fd4f249936d4bca33e40028a4a05da0
kirrane <36404451+kirrane@users.noreply.github.com>
2018-02-27 12:21:35 +0000

    Add files via upload

---
+++ Addition.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Addition {
+	public static void main(String[]args){
+	Scanner input = new Scanner(System.in);
+
+	int total = 0;
+
+	System.out.println("Enter numbers (ctrl-z to end)");
+
+	while(input.hasNext()){
+	total += input.nextInt();
+	}
+
+	System.out.println("Total is: "+total);
+ }
+}
